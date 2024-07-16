b660c2c12837b81ddab8492fbd087800a7a56a4f
Johsgronstad <johsgronstad@gmail.com>
2024-07-16 11:53:37 +0200

    Changed convertCallTime to parse timezone

--- webapp/frontend/src/app/call-list/call-list.component.ts
+++ webapp/frontend/src/app/call-list/call-list.component.ts
@@ -64,6 +64,17 @@ export class CallListComponent {
   }
 
   convertCallTime(time: string) {
-    return time.replace("T"," ").replace("Z","")
+    const date = new Date(time);
+    
+    const pad = (number: number) => number < 10 ? '0' + number : number;
+    
+    const day = pad(date.getDate());
+    const month = pad(date.getMonth() + 1);
+    const year = date.getFullYear();
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+    
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
   }
 }
